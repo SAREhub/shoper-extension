1399346736026d4bfcaea852bac58bc1527cf139
jarekkowol <jarekkowol@gmail.com>
2018-12-07 10:46:59 +0100

    add tests context runner (checkout steps)

--- src/context_runner.spec.js
+++ src/context_runner.spec.js
@@ -23,6 +23,54 @@ describe('Context tests', () => {
         expect(contexts.Product).toHaveBeenCalledWith(100, frontApi, sareWebApi);
     });
 
+    it('should execute registration context', () => {
+        const shop = {pageType: 'shop_basket_address'};
+        const frontApi = {};
+        const sareWebApi = jasmine.createSpyObj('SareWebApi', ['cartRegistration']);
+        const contexts = jasmine.createSpyObj('Contexts', ['Registration']);
+
+        const runner = SAREhub.ContextRunner(shop, frontApi, sareWebApi, contexts);
+        runner.dispatch();
+
+        expect(contexts.Registration).toHaveBeenCalledWith(sareWebApi);
+    });
+
+    it('should execute delivery and payment context', () => {
+        const shop = {pageType: 'shop_basket_shipping_payment'};
+        const frontApi = {};
+        const sareWebApi = jasmine.createSpyObj('SareWebApi', ['cartDeliveryPayment']);
+        const contexts = jasmine.createSpyObj('Contexts', ['DeliveryPayment']);
+
+        const runner = SAREhub.ContextRunner(shop, frontApi, sareWebApi, contexts);
+        runner.dispatch();
+
+        expect(contexts.DeliveryPayment).toHaveBeenCalledWith(sareWebApi);
+    });
+
+    it('should execute confirm context', () => {
+        const shop = {pageType: 'shop_basket_step3'};
+        const frontApi = {};
+        const sareWebApi = jasmine.createSpyObj('SareWebApi', ['cartConfirm']);
+        const contexts = jasmine.createSpyObj('Contexts', ['Confirm']);
+
+        const runner = SAREhub.ContextRunner(shop, frontApi, sareWebApi, contexts);
+        runner.dispatch();
+
+        expect(contexts.Confirm).toHaveBeenCalledWith(sareWebApi);
+    });
+
+    it('should execute purchased context', () => {
+        const shop = {pageType: 'shop_basket_done'};
+        const frontApi = {};
+        const sareWebApi = jasmine.createSpyObj('SareWebApi', ['cartPurchased']);
+        const contexts = jasmine.createSpyObj('Contexts', ['Purchased']);
+
+        const runner = SAREhub.ContextRunner(shop, frontApi, sareWebApi, contexts);
+        runner.dispatch();
+
+        expect(contexts.Purchased).toHaveBeenCalledWith(sareWebApi);
+    });
+
     it('should return false when context does not exist', () => {
         const runner = SAREhub.ContextRunner({pageType: 'not_supported_type'}, {}, {});
 
