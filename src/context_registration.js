155a36f238a26966c80406b2ba419711de64f054
Adrian Pietka <adrian@pietka.me>
2018-12-13 12:11:20 +0100

    Send checkout initialized on registration context

--- src/context_registration.js
+++ src/context_registration.js
@@ -1,5 +1,6 @@
 SAREhub.Contexts.Registration = function (_sareWebApi) {
     return function init() {
+        _sareWebApi.checkoutInitialized();
         _sareWebApi.cartRegistration();
     }
 };
--- src/context_registration.spec.js
+++ src/context_registration.spec.js
@@ -1,12 +1,13 @@
 describe('Context registration tests', () => {
     describe('init', () => {
         it('should call request to SAREweb API when checkout registration', () => {
-            const sareWebApi = jasmine.createSpyObj('SareWebApi', ['cartRegistration']);
+            const sareWebApi = jasmine.createSpyObj('SareWebApi', ['cartRegistration', 'checkoutInitialized']);
             const registrationContext = SAREhub.Contexts.Registration(sareWebApi);
 
             registrationContext();
 
             expect(sareWebApi.cartRegistration).toHaveBeenCalledTimes(1);
+            expect(sareWebApi.checkoutInitialized).toHaveBeenCalledTimes(1);
         });
     });
 });
